1866a0a30c878c465b2b66ed8b30600488084f52
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2024-10-30 00:59:59 -0600

    add properties, classes, csv files

---
+++ Reservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Reservation
+    {
+        private string reservationCode;
+        private string flightCode;
+        private string airlineName;
+        private double cost;
+        private string name; // customer name
+        private string citizenship;
+        private bool status; // active or inactive
+
+        public string ReservationCode { get => reservationCode; set => reservationCode = value; }
+        public string FlightCode { get => flightCode; set => flightCode = value; }
+        public string AirlineName { get => airlineName; set => airlineName = value; }
+        public double Cost { get => cost; set => cost = value; }
+        public string Name { get => name; set => name = value; }
+        public string Citizenship { get => citizenship; set => citizenship = value; }
+        public bool Status { get => status; set => status = value; }
+
+        public Reservation(string reservationCode, string flightCode, string airlineName, double cost, string name, string citizenship, bool status)
+        {
+            this.reservationCode = reservationCode;
+            this.flightCode = flightCode;
+            this.airlineName = airlineName;
+            this.cost = cost;
+            this.name = name;
+            this.citizenship = citizenship;
+            this.status = status;
+        }
+    }
+}
---
+++ ReservationManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class ReservationManager
+    {
+        //put the methods here as needed
+        //llist of flights objects
+        //make objects and add them to list
+        // use using?? for accessing file
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\..\\Resources\\res\\flights.csv");
+
+        public static List<Flight> flights = new List<Flight>();
+
+        public ReservationManager() 
+        {
+            populateFlights();
+        }
+
+        private void populateFlights()
+        {
+            Flight flight; // do i need this line??
+
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                string[] fields = line.Split(',');
+                string flightCode = fields[0];
+                string airlineName = fields[1];
+                string originAirport = fields[2];
+                string destinationAirport = fields[3];
+                string weekDate = fields[4];
+                DateTime time = DateTime.Parse(fields[5]);
+                int seats = int.Parse(fields[6]);
+                double cost = double.Parse(fields[7]);
+
+                flight = new Flight(flightCode, airlineName, originAirport, destinationAirport, weekDate, time, seats, cost);
+                flights.Add(flight);
+            }
+        }
+
+        public static List<Flight> getFlights() // method to get list of flight
+            { 
+                return flights; 
+            }
+    }            
+
+}
